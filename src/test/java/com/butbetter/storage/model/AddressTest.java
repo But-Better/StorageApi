4f8690c1ed4cad5b80dc7e7915fdd8980ab33f7d
note <kitsu-geissler@t-online.de>
2021-11-19 19:52:37 +0100

    STOR-21 add test for hash code generation on potential null value

---
+++ src/test/java/com/butbetter/storage/model/AddressTest.java
@@ -0,0 +1,22 @@
+package com.butbetter.storage.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressTest {
+
+	private Address testAddress;
+
+	@Test
+	void testNullHashCodeGeneration() {
+		testAddress = new Address("a", "a", "a", "a", "a", null);
+		try {
+			testAddress.hashCode();
+		} catch (Exception ex) {
+			fail();
+		}
+	}
+}
