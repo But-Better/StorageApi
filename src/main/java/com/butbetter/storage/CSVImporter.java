881934191bb9e95b0feb014d66e86f21519010da
note <kitsu-geissler@t-online.de>
2021-11-20 13:58:30 +0100

    STOR-12 create and implement CSVImporter

---
+++ src/main/java/com/butbetter/storage/CSVImporter.java
@@ -0,0 +1,27 @@
+package com.butbetter.storage;
+
+import com.butbetter.storage.model.ProductInformation;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+/**
+ * File-Importer for a class
+ */
+@Service
+public class CSVImporter {
+
+	private static Logger logger = LoggerFactory.getLogger(CSVImporter.class);
+
+	public List<ProductInformation> getFromCSV(File file) throws FileNotFoundException {
+		return new CsvToBeanBuilder<ProductInformation>(new FileReader(file))
+				.withType(ProductInformation.class).build().parse();
+	}
+
+}
