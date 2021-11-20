ebb877d68061c2e6d56778ec115af9a6be851249
note <kitsu-geissler@t-online.de>
2021-11-20 14:43:02 +0100

    STOR-12 test Address bean validator

---
+++ src/test/java/com/butbetter/storage/customConverter/BeanAddressConverterTest.java
@@ -0,0 +1,23 @@
+package com.butbetter.storage.customConverter;
+
+import com.butbetter.storage.model.Address;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeanAddressConverterTest {
+
+	@Test
+	void generalConvert() {
+		BeanAddressConverter<String, String> converter = new BeanAddressConverter<>();
+		String input = "Address{uuid=null, name='a', companyName='a', street='a', city='a', postCode='a', country='a'}";
+		Address address = new Address("a", "a", "a", "a", "a", "a");
+		try {
+			assertEquals(address, converter.convert(input));
+		} catch (CsvDataTypeMismatchException | CsvConstraintViolationException e) {
+			fail();
+		}
+	}
+}
