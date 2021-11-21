daa15516f3c167c46edd4560f308b1dafc6d61d8
note <kitsu-geissler@t-online.de>
2021-11-21 15:15:45 +0100

    STOR-10 enable Configuration Properties with the StorageProperties class

--- src/main/java/com/butbetter/storage/StorageApplication.java
+++ src/main/java/com/butbetter/storage/StorageApplication.java
@@ -1,9 +1,12 @@
 package com.butbetter.storage;
 
+import com.butbetter.storage.FileUpload.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class StorageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StorageApplication.class, args);
