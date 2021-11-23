557e6ed53fd7fcbd1c71cc39316f768509783092
note <kitsu-geissler@t-online.de>
2021-11-23 20:07:57 +0100

    STOR-10 add informative logger messages

--- src/main/java/com/butbetter/storage/FileUpload/CSVStorageService.java
+++ src/main/java/com/butbetter/storage/FileUpload/CSVStorageService.java
@@ -27,23 +27,27 @@ public class CSVStorageService implements StorageService {
 
 	@Autowired
 	public CSVStorageService(StorageProperties properties) {
+		logger.info("Location: \"" + properties.getLocation() + "\" is getting used for the save location");
 		this.rootLocation = Paths.get(properties.getLocation());
 	}
 
 	@Override
 	public void init() throws StorageException {
+		logger.info("initializing Storage");
 		try {
 			Files.createDirectories(rootLocation);
 		}
 		catch (IOException e) {
-			String message = "Could not initialize storage";
+			String message = "Could not initialize storage, directory at \"" + rootLocation + "\" could not be created";
 			logger.error(message);
 			throw new StorageException(message, e);
 		}
+		logger.info("initialisation complete");
 	}
 
 	@Override
 	public void store(MultipartFile file) throws StorageException {
+		logger.info("storing " + file.getOriginalFilename());
 		try {
 			if (file.isEmpty()) {
 				String message = "Failed to store empty file";
@@ -60,6 +64,8 @@ public class CSVStorageService implements StorageService {
 				logger.error(message);
 				throw new StorageException(message);
 			}
+
+
 			try (InputStream inputStream = file.getInputStream()) {
 				Files.copy(inputStream, destinationFile,
 						StandardCopyOption.REPLACE_EXISTING);
@@ -70,30 +76,35 @@ public class CSVStorageService implements StorageService {
 			logger.error(message);
 			throw new StorageException(message, e);
 		}
+		logger.info("stored " + file.getOriginalFilename());
 	}
 
 	@Override
 	public Stream<Path> loadAll() throws StorageException {
+		logger.info("loading all");
 		try {
-			return Files.walk(this.rootLocation, 1)
+			Stream<Path> paths = Files.walk(this.rootLocation, 1)
 					.filter(path -> !path.equals(this.rootLocation))
 					.map(this.rootLocation::relativize);
+			logger.info("loaded all");
+			return paths;
 		}
 		catch (IOException e) {
 			String message = "Failed to read stored files";
 			logger.error(message);
 			throw new StorageException(message, e);
 		}
-
 	}
 
 	@Override
 	public Path load(String filename) {
+		logger.info("loading " + filename);
 		return rootLocation.resolve(filename);
 	}
 
 	@Override
 	public Resource loadAsResource(String filename) throws StorageFileNotFoundException {
+		logger.info("loading " + filename);
 		try {
 			Path file = load(filename);
 			Resource resource = new UrlResource(file.toUri());
@@ -116,7 +127,9 @@ public class CSVStorageService implements StorageService {
 
 	@Override
 	public void deleteAll() {
+		logger.info("removing all known files");
 		FileSystemUtils.deleteRecursively(rootLocation.toFile());
+		logger.info("removed all known files");
 	}
 }
 
