445ea8163086ea2da1c0e61d409436d2e7b16a9c
note <kitsu-geissler@t-online.de>
2021-11-26 08:42:12 +0100

    STOR-10 add Logger to Configuration

--- src/main/java/com/butbetter/storage/FileUpload/StorageConfiguration.java
+++ src/main/java/com/butbetter/storage/FileUpload/StorageConfiguration.java
@@ -1,16 +1,23 @@
 package com.butbetter.storage.FileUpload;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageConfiguration {
+
+	private final Logger logger = LoggerFactory.getLogger(StorageConfiguration.class);
+
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
+			logger.info("initialize Storage");
 			storageService.deleteAll();
 			storageService.init();
+			logger.info("done Storage initialization");
 		};
 	}
 }
