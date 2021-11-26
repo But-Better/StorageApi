510dfb9ee00e0372e8a54ccb9ae026965df83d35
note <kitsu-geissler@t-online.de>
2021-11-26 08:39:38 +0100

    STOR-10 remove useless Exception Constructor

--- src/main/java/com/butbetter/storage/FileUpload/StorageFileNotFoundException.java
+++ src/main/java/com/butbetter/storage/FileUpload/StorageFileNotFoundException.java
@@ -1,9 +1,6 @@
 package com.butbetter.storage.FileUpload;
 
 public class StorageFileNotFoundException extends Throwable {
-	public StorageFileNotFoundException() {
-		super();
-	}
 
 	public StorageFileNotFoundException(String message) {
 		super(message);
