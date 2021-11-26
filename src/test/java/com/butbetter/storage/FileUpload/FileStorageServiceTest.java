4c25c8c219db7f59202c4b50901ee526f66b9b10
note <kitsu-geissler@t-online.de>
2021-11-26 18:45:56 +0100

    STOR-10 refactoring of class-names

--- src/test/java/com/butbetter/storage/FileUpload/FileStorageServiceTest.java
+++ src/test/java/com/butbetter/storage/FileUpload/FileStorageServiceTest.java
@@ -1,6 +1,6 @@
 package com.butbetter.storage.FileUpload;
 
-import com.butbetter.storage.CSV.CSVImporter;
+import com.butbetter.storage.CSV.CSVImportService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,28 +12,32 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-class CSVStorageServiceTest {
+import static org.mockito.Mockito.mock;
+
+class FileStorageServiceTest {
 
 	private static final String BASE_PATH = "src/test/resources/";
 
-	private CSVImporter converter;
 	private StorageProperties property;
 
-	private CSVStorageService service;
+	private FileStorageService service;
+	private CSVImportService importer;
 
 	private MultipartFile file;
-	private final String fileName = "test.csv";
+
 	@BeforeEach
 	void setUp() throws IOException {
-		converter = new CSVImporter();
+		importer = mock(CSVImportService.class);
 		property = new StorageProperties(BASE_PATH + "/csvs/");
+
+		String fileName = "test.csv";
 
 		if (!new File(BASE_PATH + fileName).exists()) {
 			throw new FileNotFoundException(fileName + " test file couldn't be found in " + BASE_PATH);
 		}
-		file = new MockMultipartFile("testFile", new FileInputStream(BASE_PATH + fileName));
+		file = new MockMultipartFile(fileName, new FileInputStream(BASE_PATH + fileName));
 
-		service = new CSVStorageService(property, converter);
+		service = new FileStorageService(property, importer);
 	}
 
 	@AfterEach
