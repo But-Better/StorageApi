f02c03438ddb150e1472d07d885eae4112c70fbe
note <kitsu-geissler@t-online.de>
2021-11-26 08:37:12 +0100

    STOR-10 add Logger to Controller

--- src/main/java/com/butbetter/storage/FileUpload/CSVFileUploadController.java
+++ src/main/java/com/butbetter/storage/FileUpload/CSVFileUploadController.java
@@ -1,26 +1,27 @@
 package com.butbetter.storage.FileUpload;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 public class CSVFileUploadController {
 
+	private final Logger logger = LoggerFactory.getLogger(CSVFileUploadController.class);
 	private final StorageService storageService;
 
 	@Autowired
@@ -30,6 +31,7 @@ public class CSVFileUploadController {
 
 	@GetMapping("/")
 	public ResponseEntity<List<Path>> listUploadedFiles() throws IOException {
+		logger.info("requested all file paths");
 		return new ResponseEntity<>(storageService.loadAll().collect(Collectors.toList()), HttpStatus.OK);
 	}
 
@@ -45,7 +47,7 @@ public class CSVFileUploadController {
 	@PostMapping("/")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
 	                               RedirectAttributes redirectAttributes) throws StorageException {
-
+		logger.info("file upload started, saving: " + file.getOriginalFilename());
 		storageService.store(file);
 		redirectAttributes.addFlashAttribute("message",
 				"You successfully uploaded " + file.getOriginalFilename() + "!");
