185b32a119cc7d6a5c303a846298d38d1ddfb5e7
note <kitsu-geissler@t-online.de>
2021-11-27 19:34:30 +0100

    STOR-6 update StorageApplication

--- src/main/java/com/butbetter/storage/StorageApplication.java
+++ src/main/java/com/butbetter/storage/StorageApplication.java
@@ -1,6 +1,6 @@
 package com.butbetter.storage;
 
-import com.butbetter.storage.FileUpload.StorageProperties;
+import com.butbetter.storage.FileUpload.Properties.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
