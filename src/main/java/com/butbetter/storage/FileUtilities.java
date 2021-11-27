d1555a3489445fc7c2bd7dd70a68902b079f5736
note <kitsu-geissler@t-online.de>
2021-11-27 19:33:01 +0100

    STOR-6 add File-Read-Utility for testing File Contents

---
+++ src/main/java/com/butbetter/storage/FileUtilities.java
@@ -0,0 +1,20 @@
+package com.butbetter.storage;
+
+import java.io.*;
+
+public class FileUtilities {
+	/**
+	 * method to read an entire file unsafely and give back the contents
+	 * @param file file handle to read from
+	 * @return String content
+	 * @throws IOException thrown, if file couldn't be read
+	 */
+	public static String readEntireFile(File file) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		StringBuilder sb = new StringBuilder();
+		while (br.ready()) {
+			sb.append(br.readLine());
+		}
+		return sb.toString();
+	}
+}
