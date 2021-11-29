6b4df5a6c21b94314c18f1565dfe1c4fa00e8f96
note <kitsu-geissler@t-online.de>
2021-11-29 17:38:05 +0100

    STOR-6 update ProductRepository

--- src/main/java/com/butbetter/storage/ProductRepository.java
+++ src/main/java/com/butbetter/storage/ProductRepository.java
@@ -1,35 +1,10 @@
 package com.butbetter.storage;
 
 import com.butbetter.storage.model.ProductInformation;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
-public interface ProductRepository extends CrudRepository<ProductInformation, UUID> {
-	
-	/*void deleteAllByIdInBatch(Iterable<ID> ids);
-	void deleteAllInBatch();
-	void deleteAllInBatch(Iterable<ProductInformation> entities);
-	List<P> findAll();
+public interface ProductRepository extends JpaRepository<ProductInformation, UUID> {
 
-	<S extends P>
-	List<S> findAll(Example<S> example);
-	<S extends P>
-	List<S> findAll(Example<S> example, Sort sort);
-
-	List<P> findAll(Sort sort);
-	List<P> findAllById(Iterable<UUID> ids);
-
-	void flush();
-	P getById(UUID id);
-
-
-
-	<S extends P>
-	List<S> saveAllAndFlush(Iterable<S> entities);
-
-	<S extends P>
-	S saveAndFlush(S entity);*/
 }
