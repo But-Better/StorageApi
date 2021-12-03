c60f5f258959c8c57577701d94a8e8f62a7f6602
kev <kevHTW@gmx.de>
2021-12-03 14:56:11 +0100

    STOR-20 update application.properties

--- src/main/java/com/butbetter/storage/service/StorageService.java
+++ src/main/java/com/butbetter/storage/service/StorageService.java
@@ -17,28 +17,63 @@ import java.util.UUID;
 public class StorageService {
 
     private static final Logger logger = LoggerFactory.getLogger(StorageService.class);
+
     private static final String couldNotfoundUser = "User not found with the UUID";
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+    private final AddressRepository addressRepository;
 
+    @Autowired
+    public StorageService(ProductRepository productRepository, AddressRepository addressRepository) {
+        this.productRepository = productRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    /**
+     * Get all {@link ProductInformation} value
+     *
+     * @return
+     */
     public List<ProductInformation> all() {
         return this.productRepository.findAll();
     }
 
+    /**
+     * Get One {@link ProductInformation} value
+     *
+     * @param id
+     * @return
+     * @throws ProductInformationNotFoundException
+     */
     public ProductInformation one(UUID id) throws ProductInformationNotFoundException {
         return this.productRepository.findById(id)
                 .orElseThrow(() -> new ProductInformationNotFoundException(couldNotfoundUser, id));
     }
 
+    /**
+     * Create a new {@link ProductInformation} value
+     *
+     * @param productInformation = {@link ProductInformation}
+     * @throws NullPointerException = is checked by ValidateANewProductInformation
+     */
     public void newProductInformation(@NotNull ProductInformation productInformation) throws NullPointerException {
         this.ValidateANewProductInformation(productInformation);
+        this.addressRepository.save(productInformation.getAddress());
         this.productRepository.save(productInformation);
     }
 
+    /**
+     * Validate the input of {@link ProductInformation}
+     *
+     * @param productInformation = {@link ProductInformation}
+     * @throws NullPointerException = if Address or DeliveryTime null
+     * @throws IllegalArgumentException = if amount smaller then zero
+     */
     private void ValidateANewProductInformation(ProductInformation productInformation) throws NullPointerException, IllegalArgumentException {
         if (productInformation.getAddress() == null || productInformation.getDeliveryTime() == null) {
-            throw new NullPointerException("Address or DeliveryTime is null");
+            String message = "Address or DeliveryTime is null";
+            logger.error(message);
+            throw new NullPointerException(message);
         }
 
         if (productInformation.getAmount() <= 0) {
