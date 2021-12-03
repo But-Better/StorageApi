9c59db437dfb7b00206398a4e5718dd5b16afa5b
kev <kevHTW@gmx.de>
2021-12-03 15:50:19 +0100

    STOR-8 rename AddressTests

--- src/test/java/com/butbetter/storage/model/AddressTests.java
+++ src/test/java/com/butbetter/storage/model/AddressTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class AddressTest {
+class AddressTests {
 
     private Address testAddress;
 
