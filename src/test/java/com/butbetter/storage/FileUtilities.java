66b4f01c35d92494004d608b9c7cb0bc22f84b03
note <kitsu-geissler@t-online.de>
2021-12-03 18:23:21 +0100

    STOR-10 add warning to the unsafe file read method for tests

--- src/test/java/com/butbetter/storage/FileUtilities.java
+++ src/test/java/com/butbetter/storage/FileUtilities.java
@@ -5,6 +5,9 @@ import java.io.*;
 public class FileUtilities {
 	/**
 	 * method to read an entire file unsafely and give back the contents
+	 *
+	 * WARNING: DON'T USE THIS OUTSIDE OF TESTS
+	 *
 	 * @param file file handle to read from
 	 * @return String content
 	 * @throws IOException thrown, if file couldn't be read
