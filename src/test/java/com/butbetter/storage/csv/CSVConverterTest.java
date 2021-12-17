61fdadd0c99292b3f944a9fc731c6722193996d9
notefox <kitsu-geissler@t-online.de>
2021-12-17 13:01:42 +0100

    STOR-25 add forgotten uuid to Address

--- src/test/java/com/butbetter/storage/csv/CSVConverterTest.java
+++ src/test/java/com/butbetter/storage/csv/CSVConverterTest.java
@@ -37,7 +37,7 @@ class CSVConverterTest {
 
 		productInformation = new ArrayList<>();
 
-		productInformation.add(new ProductInformation(UUID.randomUUID(), OffsetDateTime.now(), 5, new Address("a", "a", "a", "a", "a", "a")));
+		productInformation.add(new ProductInformation(UUID.randomUUID(), OffsetDateTime.now(), 5, new Address(UUID.randomUUID(), "a", "a", "a", "a", "a", "a")));
 		Writer writer = new FileWriter(file);
 		StatefulBeanToCsv<ProductInformation> beanToCsv = new StatefulBeanToCsvBuilder<ProductInformation>(writer).build();
 		beanToCsv.write(productInformation);
